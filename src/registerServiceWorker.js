b19da9de5ff1e212b0788c748fbc9e8a920b1e85
Taylor Fuqua <tfuqua@gwu.edu>
2017-12-06 00:03:31 -0500

    Fix sw.js config

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -28,7 +28,7 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/static/js/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
